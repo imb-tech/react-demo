dcd9ec25b58ca7359252fbaa229bee8bbce6f5df
Doniyor Eshmamatov <edoniyorbekdev@gmail.com>
2025-05-21 23:44:58 +0500

    create form

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,13 @@
+import { useNavigate } from "react-router";
 import { Button } from "../ui/button";
 
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <header className="py-2 shadow-sm">
       <div className="flex items-center justify-between container mx-auto ">
         <h2 className="text-xl">Logo</h2>
-        <Button>Create todo</Button>
+        <Button onClick={() => navigate("/create")}>Create todo</Button>
       </div>
     </header>
   );
--- src/pages/create-todo/index.tsx
+++ src/pages/create-todo/index.tsx
@@ -1,3 +1,51 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const FormSchema = z.object({
+  title: z.string().min(2, {
+    message: "Title is required",
+  }),
+});
+
 export default function CreateTodoPage() {
-  return <div>CreateTodoPage</div>;
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 }
